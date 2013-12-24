6ae19ccdf2b485137a719d2ad3e904609d4f3a7b
RAD <radkiller@yandex.ru>
2013-12-24 16:49:57 +0200

    add b-siteSearch

---
+++ b-siteSearch/b-siteSearch.php
@@ -0,0 +1,7 @@
+<div class="l-siteSearch">
+  <form class="b-siteSearch" role="search" method="post" action="#">
+    <label class="b-siteSearch__label g-visuallyhidden" for="search-text">Search:</label>
+    <input class="b-siteSearch__input" type="search" spellcheck="true" value="" name="search" id="search-text" />
+    <input class="b-siteSearch__button" type="submit" value="Search" />
+  </form>
+</div>
