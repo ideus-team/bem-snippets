6b60a6c293c4e50df33f174c50f51ad9ca8f1e60
RAD <radkiller@yandex.ru>
2014-02-06 13:16:39 +0200

    Add share42

---
+++ b-socialButtons/b-socialButtons.php
@@ -0,0 +1,6 @@
+<div class="b-socialButtons">
+  <div class="b-socialButtons__inner">
+    <div class="b-socialButtons__description">Рассказать друзьям</div>
+    <div class="b-socialButtons__content share42init" data-url="http://<?php echo ($_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']); ?>" data-title="<?php echo $ogTitle; ?>" data-description="<?php echo $ogDescription; ?>" data-image="<?php echo $SITE_URL; ?>img/social/og-image.png" data-path="img/social/" data-zero-counter="1"></div>       
+  </div>
+</div>
