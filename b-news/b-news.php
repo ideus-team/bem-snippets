d1e531c78d06724055c9edb1310b99431986ff5e
pasha_bondarenko <bpv.pashtet@gmail.com>
2014-02-14 13:47:45 +0200

    b-news

---
+++ b-news/b-news.php
@@ -0,0 +1,18 @@
+<div class="l-news">
+    <div class="b-news">
+        <article class="b-news__item" itemscope itemtype="http://schema.org/Article">
+        <h3 class="b-news__header" itemprop="name">?????? ???????</h3>
+        <div itemprop="image" class="b-news__img">
+            <img src="" alt="">
+        </div>
+        <div class="b-news__content">
+            <section itemprop="articleBody">
+                ???? ????? ?????? ???????
+            </section>
+            <section class="b-news__author" itemprop="author" itemscope itmetype="http://schema.org/Person">
+                <span itemprop="additionalName">???? ?.</span>
+            </section>            
+        </div>
+        </article>        
+    </div>
+</div>
