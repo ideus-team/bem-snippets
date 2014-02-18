1f3a4de13644c4c5d97af273416fe8a483e80a4b
Ann <iamcodergirl@gmail.com>
2014-02-18 14:48:23 +0200

    microdataOrganization, microdataEvent

---
+++ b-event/b-event.php
@@ -0,0 +1,17 @@
+<div class="l-event">
+  <ul itemscope itemtype="http://schema.org/Event" class="b-event">
+    <li class="b-event__item">
+      <div class="b-event__figure">
+        <img itemprop="image" class="b-event__img" alt="" src="">
+      </div>
+      <div class="b-eventContent">
+        <div class="b-event__hgroup">
+          <h3 itemprop="name" class="b-event__header"></h3>
+          <time itemprop="startDate" datetime="2011-05-08T19:30" class="b-events__date"></time>
+        </div>
+        <div itemprop="description" class="b-eventText"></div>
+        <a itemprop="url" class="b-event__moreLink" target="_blank"  href=""></a>
+      </div>
+    </li>
+  </ul>
+</div>
---
+++ microdataOrganization/microdataOrganization.php
@@ -0,0 +1,13 @@
+ <address class="" itemscope itemtype="http://schema.org/Organization">
+  <a itemprop="name" class="" rel="me" href="http://arizonabouncearound.com">arizona bounce around</a>
+  <span itemprop="address" itemscope itemtype="http://schema.org/PostalAddress" class="">
+    <span itemprop="streetAddress" class="">1717 N 77th Street Suite 20</span>
+    <span itemprop="addressLocality" class="">Scottsdale</span>,
+    <abbr itemprop="addressRegion" class="" title="Arizona">AZ</abbr>
+    <span itemprop="postalCode" class="">85257</span>
+    <span itemprop="addressCountry" class="">USA</span><br />
+    Phone: <span itemprop="telephone" class="" title="480-874-3470">480-874-3470</span>
+    Fax: <span itemprop="faxNumber" class="" title="480-874-3470">480-874-3470</span><br />
+    E-mail: <a itemprop="email" class="" href="mailto:info@azbouncearound.com">info@azbouncearound.com</a>
+  </span>
+</address>
