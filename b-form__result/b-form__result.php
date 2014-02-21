1dd212453f9bbe5c3abe191f107da0d2ca673552
Ann <iamcodergirl@gmail.com>
2014-02-21 18:39:32 +0200

    b-form__result

---
+++ b-form__result/b-form__result.php
@@ -0,0 +1,4 @@
+<div class="b-form__result">
+  <div class="b-form__resultLoader"></div>
+  <div class="b-form__resultTxt">В базе найдено <span class="b-form__resultNumber">23</span> рейса, удовлетворяющих выбранными критериями</div>
+</div>
