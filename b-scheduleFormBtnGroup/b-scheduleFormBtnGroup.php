e38ed2720082ebd6e7aa9359dbcc72f125d19f2f
Ann <iamcodergirl@gmail.com>
2014-02-21 17:10:57 +0200

    b-scheduleFormBtnGroup.php

--- b-scheduleFormBtnGroup/b-scheduleFormBtnGroup.php
+++ b-scheduleFormBtnGroup/b-scheduleFormBtnGroup.php
@@ -1,18 +1,8 @@
-<div class="l-news">
-    <div class="b-news">
-        <article class="b-news__item" itemscope itemtype="http://schema.org/Article">
-        <h3 class="b-news__header" itemprop="name">?????? ???????</h3>
-        <div itemprop="image" class="b-news__img">
-            <img src="" alt="">
-        </div>
-        <div class="b-news__content">
-            <section itemprop="articleBody">
-                ???? ????? ?????? ???????
-            </section>
-            <section class="b-news__author" itemprop="author" itemscope itmetype="http://schema.org/Person">
-                <span itemprop="additionalName">???? ?.</span>
-            </section>            
-        </div>
-        </article>        
-    </div>
-</div>
+<ul class="b-scheduleFormBtnGroup -type_1">
+  <li class="b-scheduleFormBtnGroup__btn -num_1 -type_1 -state_current j-oneWayBtn">
+    <a class="b-scheduleFormBtnGroup__link" data-oneway="0">Туда и обратно</a>
+  </li>
+  <li class="b-scheduleFormBtnGroup__btn -num_2 -type_1 j-oneWayBtn">
+    <a class="b-scheduleFormBtnGroup__link" data-oneway="1">В одну сторону</a>
+  </li>
+</ul>
