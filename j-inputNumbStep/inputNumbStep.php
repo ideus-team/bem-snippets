479ba5dd1e09f43ce48280d37d463f663b1efbeb
Ann <iamcodergirl@gmail.com>
2014-03-05 15:01:48 +0200

    j-inputNumbStep

---
+++ j-inputNumbStep/inputNumbStep.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>j-inputNumbStep</title>
+  <link rel="stylesheet" href="inputNumbStep.css"/>
+  
+   <!--[if lte IE 8]>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>    
+  <![endif]-->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+  <script src="js/plugins/jquery.fs.stepper.js" type="text/javascript"></script>
+<script src="js/main.js" type="text/javascript"></script>
+</head>
+<body>
+<main class="l-contentText" role="main">
+  <input class="b-number" type="number" min="0" max="20" step="1" />
+</main>	
+</body>
+
+</html>
+
+
--- j-zoomProduct/js/main.js
+++ j-zoomProduct/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-  imgZoom();
+imgZoom();
 });
+
 function imgZoom() {
 //initiate the plugin and pass the id of the div containing gallery images
 $("#zoom").elevateZoom({gallery:'b-productGallery', cursor: 'pointer', galleryActiveClass: 'active',  tintColour:'#6a2e92', tintOpacity:'0.5', imageCrossfade: true, loadingIcon: 'img/spinner.gif', tint:true}); 
