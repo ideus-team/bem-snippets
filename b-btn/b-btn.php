844264b41be85b51660107656f3ec23c5385006a
Ann <iamcodergirl@gmail.com>
2014-04-03 16:49:58 +0300

    кнопка с иконкой

--- b-btn/b-btn.php
+++ b-btn/b-btn.php
@@ -1,13 +1,2 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>Button</title>
-  <link rel="stylesheet" href="sass/style.css"/>
-</head>
-<body>
-	<a href="#" class="b-btn">Button</a>
-	<button class="b-btn">Button</button>
-	<input type="submit" value="Sbmit" class="b-btn"/>
-</body>
-</html>
+<a href="#" class="b-btn">Button</a>
+<a href="#" class="b-btn -ico_play">Button</a>
