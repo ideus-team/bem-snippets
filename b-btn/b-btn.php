1690ff0d563d8dbe4d19f1c4006b52406a9fcc89
Ann <iamcodergirl@gmail.com>
2014-04-03 16:25:19 +0300

    b-btn

---
+++ b-btn/b-btn.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Button</title>
+  <link rel="stylesheet" href="sass/style.css"/>
+</head>
+<body>
+	<a href="#" class="b-btn">Button</a>
+	<button class="b-btn">Button</button>
+	<input type="submit" value="Sbmit" class="b-btn"/>
+</body>
+</html>
