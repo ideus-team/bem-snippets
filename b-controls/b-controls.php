fa20dda664dbd4d6ee0c115cecbf0b0f0461eab8
Amnis <evgeny.makeev@ideus.biz>
2014-04-04 19:09:08 +0300

    .b-controls

---
+++ b-controls/b-controls.php
@@ -0,0 +1,68 @@
+<div class="l-wrapper">
+
+  <form class="b-form" action="">
+    <h2 class="b-form__header">Phone Numbers</h2>
+    <div class="b-controls">
+      <div class="b-controls__control">
+        <label class="b-controls__label">
+          <input type="text" placeholder="Cell">
+          <span class="b-controls__hint">Minimum 8 characters, one of which must be a symbol (!@#$%^&/?)</span>
+        </label>
+      </div>
+      <div class="b-controls__control">
+        <input type="text" placeholder="Home">
+      </div>
+      <div class="b-controls__control">
+        <input type="text" placeholder="Direct Office">
+      </div>
+      <div class="b-controls__control">
+        <select>
+          <option value="">1</option>
+          <option value="">2</option>
+          <option value="">3</option>
+        </select>
+      </div>
+      <div class="b-controls__control">
+        <input type="text" placeholder="Other">
+      </div>
+          
+    <div class="b-controls__radio">
+      <div class="b-controls__control">
+        <label class="b-controls__label">
+          <input type="radio" name="radio">Description
+        </label>
+      </div>
+      <div class="b-controls__control">
+        <label class="b-controls__label">
+          <input type="radio" name="radio">Description
+        </label>
+      </div>
+      <div class="b-controls__control">
+        <label class="b-controls__label">
+          <input type="radio" name="radio">Description
+        </label>
+      </div>
+    </div>
+      
+    <div class="b-controls__checkboxes">
+      <div class="b-controls__control">
+        <label class="b-controls__label">
+          <input type="checkbox">Description
+        </label>
+      </div>
+      <div class="b-controls__control">
+        <label class="b-controls__label">
+          <input type="checkbox">Description
+        </label>
+      </div>
+      <div class="b-controls__control">
+        <label class="b-controls__label">
+          <input type="checkbox">Description
+        </label>
+      </div>
+    </div>
+    
+    </div>
+  </form>
+
+</div>
