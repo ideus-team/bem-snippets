f0527d227e3d7174318879dd66dd09bdd37fdd08
iamcodergirl <iamcodergirl@gmail.com>
2014-04-10 14:54:17 +0300

    b-developerCopyright

--- b-developerCopyright/b-developerCopyright.php
+++ b-developerCopyright/b-developerCopyright.php
@@ -1,3 +1,1 @@
-<div class="b-developerCopyright">
-	<a itemprop="name" href="http://net-craft.com" rel="friend" class="b-developerCopyright__link">Arizona Web Design</a>
-</div>
+<div class="b-developerCopyright"><a itemprop="name" href="http://net-craft.com" rel="friend" class="b-developerCopyright__link">Arizona Web Design</a></div>
