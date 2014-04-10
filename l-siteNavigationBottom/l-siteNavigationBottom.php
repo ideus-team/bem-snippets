7a99aeca0092d8cdec2f017a44d03630bfafb717
iamcodergirl <iamcodergirl@gmail.com>
2014-04-10 15:06:52 +0300

    l-siteNavigationBottom

---
+++ l-siteNavigationBottom/l-siteNavigationBottom.php
@@ -0,0 +1,10 @@
+<div class="l-siteNavigationBottom" role="navigation">
+	<ul class="b-bottomNavigation">
+		<li class="b-bottomNavigation__item<?php echo ($uri=='home')?' -state_current':''; ?>">
+			<a class="b-bottomNavigation__link" href="index.html">Home</a>
+		</li>
+		<li class="b-bottomNavigation__item">
+			<a class="b-bottomNavigation__link" href="#">Our People</a>
+		</li>
+	</ul>
+</div>
