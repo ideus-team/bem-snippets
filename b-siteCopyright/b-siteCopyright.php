ee9fa8841e963261b67ac8af93a310211bae0777
Ann <iamcodergirl@gmail.com>
2014-04-14 12:09:44 +0300

    ssh test

--- b-siteCopyright/b-siteCopyright.php
+++ b-siteCopyright/b-siteCopyright.php
@@ -1,3 +1,4 @@
+
 <div class="b-siteCopyright">
 	&copy; 2014 <a href="http://client-site-name.com" class="b-siteCopyright__link fn n org url work" itemprop="name" rel="me">Carter Mackenzie International</a>, All Rights Reserved
 </div>
