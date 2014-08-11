b50f8d3881859cb7d512acf04d96bccccdbc7430
AnnZhiv <iamcodergirl@gmail.com>
2014-08-11 11:51:47 +0300

    хлебные крошки

---
+++ b-breadCrumbs/b-breadCrumbs.php
@@ -0,0 +1,8 @@
+<ul class="b-breadCrumbs g-clearfix">
+    <li class="b-breadCrumbs__item">
+      <a href="#" class="b-breadCrumbs__link"></a>
+    </li>
+    <li class="b-breadCrumbs__item">
+      <a class="b-breadCrumbs__link"></a>
+    </li>
+  </ul>
