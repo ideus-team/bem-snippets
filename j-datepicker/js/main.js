4f99f43474d5c5f9bde6681c2db312a5ef4fc175
AnnZhiv <iamcodergirl@gmail.com>
2014-08-11 12:38:26 +0300

    добавила localization ru

--- j-datepicker/js/main.js
+++ j-datepicker/js/main.js
@@ -3,5 +3,28 @@ $(document).ready(function(){
 });
 
 function initDatepicker(){
-    $('.j-datepicker').datepicker();
+     $.datepicker.regional.ru = {
+      closeText: 'Закрыть',
+      prevText: '&#x3c;Пред',
+      nextText: 'След&#x3e;',
+      currentText: 'Сегодня',
+      monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+      'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+      monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
+      'Июл','Авг','Сен','Окт','Ноя','Дек'],
+      dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+      dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+      dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+      weekHeader: 'Не',
+      dateFormat: 'dd.mm.yy',
+      firstDay: 1,
+      isRTL: false,
+      showMonthAfterYear: false,
+      yearSuffix: ''};
+    $.datepicker.setDefaults($.datepicker.regional.ru);
+    
+    $('.j-datepicker').datepicker({
+      dateFormat: "d.mm.yy"
+    },$.datepicker.regional.ru);
+    
 }
