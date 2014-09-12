90a7e49cf0676ddb4133ae85cf9b515872cccd51
Igor Zenich <izenich@yandex.ua>
2014-09-12 14:48:45 +0300

    Замена alert() через fancybox-popup

---
+++ js-initCustomAlert/js-initCustomAlert.js
@@ -0,0 +1,19 @@
+function initCustomAlert() {
+  window.alert = function (message) {
+    // Сбрасываем focus у текущего элемента, чтоб например нельзя было написать текст в input, когда выведен попап.
+    $(document.activeElement).blur();
+    $.fancybox.open(
+      [{
+        content : message, // html что выводится в попап
+        closeClick : true, // закрывать попап просто кликнув по нему мышью
+      }]
+    );
+  };
+}
+
+$(document).ready(function() {
+  initCustomAlert();
+});
+
+// Пример использования
+// alert('Alert text');
