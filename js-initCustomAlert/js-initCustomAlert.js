245f568706d8e3accb882132d09c31db5b2efe75
Igor Zenich <izenich@yandex.ua>
2014-09-23 18:17:02 +0300

    Добавил возврат фокуса на активный элемент
    При закрытии fancybox фокус теперь возвращается на активный элемент. Также это фиксит работу кода вида:
    alert('Please input first name');
    $('.firstName').focus();

--- js-initCustomAlert/js-initCustomAlert.js
+++ js-initCustomAlert/js-initCustomAlert.js
@@ -1,11 +1,16 @@
 function initCustomAlert() {
   window.alert = function (message) {
-    // Сбрасываем focus у текущего элемента, чтоб например нельзя было написать текст в input, когда выведен попап.
-    $(document.activeElement).blur();
+    var activeEl = document.activeElement; // сохраняем текущих активный элемент
     $.fancybox.open(
       [{
         content : message, // html что выводится в попап
-        closeClick : true // закрывать попап просто кликнув по нему мышью
+        closeClick : true, // закрывать попап просто кликнув по нему мышью   
+        beforeLoad: function(){
+          activeEl.blur(); // сбрасываем у него фокус, чтоб например нельзя было написать текст в input, когда выведен попап.
+        },           
+        afterClose: function(){
+          activeEl.focus(); // возвращаем фокус на активный элемент
+        }    
       }]
     );
   };
