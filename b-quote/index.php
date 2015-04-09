cef7449f01ced0ff6341fb9c553e7fe4902d02f8
delka <delka@ideus.biz>
2015-04-09 17:54:41 +0300

    b-quote

---
+++ b-quote/index.php
@@ -0,0 +1,16 @@
+<blockquote class="b-quote">
+ <div class="b-quote__content">
+    <p>
+      Я построю свой лунный модуль! С блэкджеком и шлюхами. Хотя, к чёрту модуль и блэккджек. Ай, к чёрту всё…
+    </p>
+  </div>
+  <footer class="b-quote__copyright h-card">
+    <img class="b-quote__logo u-logo" src="img/blocks/b-testimonials/logo-Brand.png" alt="Brand name">
+    <cite class="b-quote__authorName p-name">
+      Бендер
+    </cite>
+    <span class="b-quote__authorDescription p-job-title">
+      Начальник отдела интернет-маркетинга
+    </span>
+  </footer>
+</blockquote>
