de40da1ae9015b0802e7159b8e0655d672115fc7
delka <delka@ideus.biz>
2015-04-14 15:38:09 +0300

    "Site Down For Maintenance" page

---
+++ b-maintenance/maintenance.php
@@ -0,0 +1,14 @@
+<?php
+header("HTTP/1.1 503 Service Temporarily Unavailable");
+header("Status: 503 Service Temporarily Unavailable");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Site Down For Maintenance</title>
+</head>
+<body>
+<h1>Site Down For Maintenance</h1>
+<p>We are currently working on the website.</p>
+</body>
+</html>
