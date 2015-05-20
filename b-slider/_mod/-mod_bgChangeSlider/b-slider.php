5867e7b3ca641cad06089ac65b4b39c3191980e7
delka <delka@ideus.biz>
2015-05-20 22:59:12 +0300

    Refactoring b-slider/_mod/-mod_bgChangeSlider Remove unnecessary Rewrite names to be consistent

--- b-slider/_mod/-mod_bgChangeSlider/b-slider.php
+++ b-slider/_mod/-mod_bgChangeSlider/b-slider.php
@@ -1,27 +1,20 @@
-<section class="-type_testimonials js-sliderBg">
-  <div class="js-sliderOverlay">
+<section class="-type_testimonials js-section">
 
     <section class="l-slider">
       <ul class="b-slider js-slider">
-        <li class="b-slider__item js-sliderItem" data-bg="img/blocks/b-testimonials/b-testimonials-bg--BrandName-1.jpg">
-          <div class="b-slider__ill">
-            <a href="#" class="b-slider__link">
-              <img src="#" alt="" class="b-slider__image">
-            </a>
+        <li class="b-slider__item js-sliderItem">
+          <div class="b-slider__text">
+            Lorem ipsum
           </div>
         </li>
-        <li class="b-slider__item js-sliderItem" data-bg="img/blocks/b-testimonials/b-testimonials-bg--BrandName-1.jpg">
-          <div class="b-slider__ill">
-            <a href="#" class="b-slider__link">
-              <img src="#" alt="" class="b-slider__image">
-            </a>
+        <li class="b-slider__item js-sliderItem">
+          <div class="b-slider__text">
+            Lorem ipsum
           </div>
         </li>
-        <li class="b-slider__item js-sliderItem" data-bg="img/blocks/b-testimonials/b-testimonials-bg--BrandName-1.jpg">
-          <div class="b-slider__ill">
-            <a href="#" class="b-slider__link">
-              <img src="#" alt="" class="b-slider__image">
-            </a>
+        <li class="b-slider__item js-sliderItem">
+          <div class="b-slider__text">
+            Lorem ipsum
           </div>
         </li>
       </ul>
@@ -31,13 +24,12 @@
         <div class="b-slider__control -type_next js-sliderControl">〉</div>
       </div>
     </section>
-  </div>
 
-       <ul class="b-testimonials__bgSlider js-bgSlider">
-         <li class="b-testimonials__background js-sliderItem" style="background-image: url(img/blocks/b-testimonials/b-testimonials-bg--AFL.jpg)"></li>
-         <li class="b-testimonials__background js-sliderItem" style="background-image: url(img/blocks/b-testimonials/b-testimonials-bg--2TBank.jpg)"></li>
-         <li class="b-testimonials__background js-sliderItem" style="background-image: url(img/blocks/b-testimonials/b-testimonials-bg--Kayak.jpg)"></li>
-         <li class="b-testimonials__background js-sliderItem" style="background-image: url(img/blocks/b-testimonials/b-testimonials-bg--OknaKomforta.jpg)"></li>
-         <li class="b-testimonials__background js-sliderItem" style="background-image: url(img/blocks/b-testimonials/b-testimonials-bg--Sovcombank.jpg)"></li>
+       <ul class="b-slider__bgSlider js-bgSlider">
+         <li class="b-slider__background js-sliderItem" style="background-image: url(img/blocks/b-testimonials/b-testimonials-bg--AFL.jpg)"></li>
+         <li class="b-slider__background js-sliderItem" style="background-image: url(img/blocks/b-testimonials/b-testimonials-bg--2TBank.jpg)"></li>
+         <li class="b-slider__background js-sliderItem" style="background-image: url(img/blocks/b-testimonials/b-testimonials-bg--Kayak.jpg)"></li>
+         <li class="b-slider__background js-sliderItem" style="background-image: url(img/blocks/b-testimonials/b-testimonials-bg--OknaKomforta.jpg)"></li>
+         <li class="b-slider__background js-sliderItem" style="background-image: url(img/blocks/b-testimonials/b-testimonials-bg--Sovcombank.jpg)"></li>
        </ul>
 </section>
