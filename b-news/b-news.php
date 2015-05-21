e38af408cc8c7da33bdfa7d863c20f162d6e5d66
delka <delka@ideus.biz>
2015-05-21 09:44:04 +0300

    Update b-news with https://github.com/pfefferle/SemPress/

--- b-news/b-news.php
+++ b-news/b-news.php
@@ -1,18 +1,47 @@
 <div class="l-news">
     <div class="b-news">
-        <article class="b-news__item" itemscope itemtype="http://schema.org/Article">
-        <h3 class="b-news__header" itemprop="name">?????? ???????</h3>
-        <div itemprop="image" class="b-news__img">
-            <img src="" alt="">
+
+      <article class="b-post">
+        <header class="b-post__header entry-header">
+          <h1 class="b-post__title entry-title p-name" itemprop="name headline">
+            <a class="b-post__url u-url url" href="http://equilytix.ideus.biz/hello-world/" title="Permalink to Hello world!" rel="bookmark" itemprop="url">
+              Hello world!
+            </a>
+            </h1>
+          <div class="b-post__published entry-meta">
+            <span class="b-post__publishedText">Posted on</span>
+            <a class="b-post__publishedLink url u-url" href="http://equilytix.ideus.biz/hello-world/" title="10:46 am" rel="bookmark">
+              <time class="b-post__publishedTime entry-date updated published dt-updated dt-published" datetime="2015-05-20T10:46:15+00:00" itemprop="dateModified">
+                May 20, 2015
+              </time>
+            </a>
+            <address class="b-post__copyright">
+              <span class="b-post__copyrightText">by</span>
+              <span class="b-post__author author p-author vcard hcard h-card" itemprop="author" itemscope="" itemtype="http://schema.org/Person">
+                <img class="b-post__authorPhoto" alt="" src="">
+                <a class="b-post__authorLink url uid u-url u-uid fn p-name" href="http://equilytix.ideus.biz/author/netcraft/" title="View all posts by netcraft" rel="author" itemprop="url">
+                  <span class="b-post__authorName" itemprop="name">
+                    netcraft
+                  </span>
+                </a>
+              </span>
+            </address>
+          </div>
+        </header>
+        <div class="b-post__content b-text entry-content e-content" itemprop="description articleBody">
+          <p>Welcome to WordPress. This is your first post. Edit or delete it, then start blogging!</p>
         </div>
-        <div class="b-news__content">
-            <section itemprop="articleBody">
-                ???? ????? ?????? ???????
-            </section>
-            <section class="b-news__author" itemprop="author" itemscope itmetype="http://schema.org/Person">
-                <span itemprop="additionalName">???? ?.</span>
-            </section>            
-        </div>
-        </article>        
+        <!-- .entry-content -->
+        <footer class="b-post__meta">
+          <span class="b-post__metaText">Posted</span>
+          <span class="b-post__metaLinks -type_category">
+            <span class="b-post__metaLinksText">in</span>
+            <a class="b-post__metaLink -type_category" href="http://equilytix.ideus.biz/category/uncategorized/" rel="category tag">Uncategorized</a>
+            <a class="b-post__commentsLink" href="http://equilytix.ideus.biz/hello-world/#comments">1 Comment</a>
+            <a class="b-post__editLink" href="http://equilytix.ideus.biz/wp-admin/post.php?post=1&amp;action=edit">Edit</a>
+          </span>
+        </footer>
+      </article>
+
     </div>
 </div>
