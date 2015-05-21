8b33101e95ebb8403c26a9b60de6440eea7bf57c
delka <delka@ideus.biz>
2015-05-21 09:56:42 +0300

    Fix validation

--- b-news/b-news.php
+++ b-news/b-news.php
@@ -1,7 +1,7 @@
 <div class="l-news">
     <div class="b-news">
 
-      <article class="b-post">
+      <article class="b-post h-entry hentry" itemscope="" itemtype="http://schema.org/BlogPosting">
         <header class="b-post__header entry-header">
           <h1 class="b-post__title entry-title p-name" itemprop="name headline">
             <a class="b-post__url u-url url" href="http://equilytix.ideus.biz/hello-world/" title="Permalink to Hello world!" rel="bookmark" itemprop="url">
@@ -18,7 +18,7 @@
             <address class="b-post__copyright">
               <span class="b-post__copyrightText">by</span>
               <span class="b-post__author author p-author vcard hcard h-card" itemprop="author" itemscope="" itemtype="http://schema.org/Person">
-                <img class="b-post__authorPhoto" alt="" src="">
+                <img class="b-post__authorPhoto u-photo photo" alt="" src="http://1.gravatar.com/avatar/4fe8599d9ec5dd15c95211db12448e6e?s=40&d=mm&r=g">
                 <a class="b-post__authorLink url uid u-url u-uid fn p-name" href="http://equilytix.ideus.biz/author/netcraft/" title="View all posts by netcraft" rel="author" itemprop="url">
                   <span class="b-post__authorName" itemprop="name">
                     netcraft
