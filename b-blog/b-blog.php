e856e32982f26a578108aa2ce0652510bd5ebf6c
delka <delka@ideus.biz>
2015-05-28 17:36:11 +0300

    b-blog

---
+++ b-blog/b-blog.php
@@ -0,0 +1,49 @@
+<div class="l-blog">
+    <div class="b-blog hfeed h-feed" itemscope="" itemtype="http://schema.org/Blog">
+
+      <article class="b-post h-entry hentry" itemprop="blogPost" itemscope="" itemtype="http://schema.org/BlogPosting">
+        <header class="b-post__header entry-header">
+          <h1 class="b-post__title entry-title p-name" itemprop="name headline">
+            <a class="b-post__url u-url url" href="http://equilytix.ideus.biz/hello-world/" title="Permalink to Hello world!" rel="bookmark" itemprop="url">
+              Hello world!
+            </a>
+            </h1>
+          <div class="b-post__published entry-meta">
+            <span class="b-post__publishedText">Posted on</span>
+            <a class="b-post__publishedLink url u-url" href="http://equilytix.ideus.biz/hello-world/" title="10:46 am" rel="bookmark">
+              <time class="b-post__publishedTime entry-date updated published dt-updated dt-published" datetime="2015-05-20T10:46:15+00:00" itemprop="dateModified">
+                May 20, 2015
+              </time>
+            </a>
+            <address class="b-post__copyright">
+              <span class="b-post__copyrightText">by</span>
+              <span class="b-post__author author p-author vcard hcard h-card" itemprop="author" itemscope="" itemtype="http://schema.org/Person">
+                <img class="b-post__authorPhoto u-photo photo" alt="" src="http://1.gravatar.com/avatar/4fe8599d9ec5dd15c95211db12448e6e?s=40&d=mm&r=g">
+                <a class="b-post__authorLink url uid u-url u-uid fn p-name" href="http://equilytix.ideus.biz/author/netcraft/" title="View all posts by netcraft" rel="author" itemprop="url">
+                  <span class="b-post__authorName" itemprop="name">
+                    netcraft
+                  </span>
+                </a>
+              </span>
+            </address>
+          </div>
+        </header>
+        <div class="b-post__summary b-text entry-summary p-summary" itemprop="description">
+          In which I extoll the virtues of using microformats.
+        </div>
+        <div class="b-post__content b-text entry-content e-content" itemprop="articleBody">
+          <p>Welcome to WordPress. This is your first post. Edit or delete it, then start blogging!</p>
+        </div>
+        <footer class="b-post__meta">
+          <span class="b-post__metaText">Posted</span>
+          <span class="b-post__metaLinks -type_category">
+            <span class="b-post__metaLinksText">in</span>
+            <a class="b-post__categoryLink" href="http://equilytix.ideus.biz/category/uncategorized/" rel="category tag">Uncategorized</a>
+            <a class="b-post__commentsLink" href="http://equilytix.ideus.biz/hello-world/#comments">1 Comment</a>
+            <a class="b-post__editLink" href="http://equilytix.ideus.biz/wp-admin/post.php?post=1&amp;action=edit">Edit</a>
+          </span>
+        </footer>
+      </article>
+
+    </div>
+</div>
