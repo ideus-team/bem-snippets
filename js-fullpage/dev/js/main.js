bbc7d9559cb96c4bdb000c37435d58a2f56ead4b
Igor Zenich <delka@ideus.biz>
2015-07-27 12:30:43 +0300

    Add js-fullpage = Single page JS (hashchange & other funcs)

---
+++ js-fullpage/dev/js/main.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+  initSectionScroll();
+});
+
+function initSectionScroll() {
+$('.js-sectionWrapper').fullpage({
+  //Custom selectors
+  sectionSelector: '.js-section',
+  
+  //Navigation
+  navigation: true,
+  navigationPosition: 'right',
+  
+  //Design
+  verticalCentered: false,
+  
+  //Scrolling
+  fitToSection: true
+  
+/*  
+  //Navigation
+        menu: false,
+        lockAnchors: false,
+        anchors:['firstPage', 'secondPage'],
+        navigation: false,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        showActiveTooltip: false,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        resize : false,
+        sectionsColor : ['#ccc', '#fff'],
+        paddingTop: '3em',
+        paddingBottom: '10px',
+        fixedElements: '#header, .footer',
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+*/
+  });
+}
