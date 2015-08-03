2f8a1a4a8f81cf87ccce90f9c0b820c2a9e828dc
Igor Zenich <delka@ideus.biz>
2015-08-03 16:24:34 +0300

    php-geo: add detect user's preferred language

--- php-geo/index.php
+++ php-geo/index.php
@@ -11,7 +11,23 @@ $geo = $SxGeo->getCity($_SERVER['REMOTE_ADDR']);
 
 $country = ($geo) ? strtolower($geo['country']['iso'])  : $COUNTRY_DEFAULT;
 $city    = ($geo) ? strtolower($geo['city']['name_en']) : $CITY_DEFAULT;
+?>
 
+<script>
+  // Detect user's preferred language (from browser & OS settings)
+  // More info: https://alicoding.com/detect-browser-language-preference-in-firefox-and-chrome-using-javascript/
+  var langPreferred = window.navigator.languages ? window.navigator.languages[0] : null;
+      langPreferred = langPreferred || window.navigator.language || window.navigator.browserLanguage || window.navigator.userLanguage;
+  if (langPreferred.indexOf('-') !== -1)
+      langPreferred = langPreferred.split('-')[0];
+
+  if (langPreferred.indexOf('_') !== -1)
+      langPreferred = langPreferred.split('_')[0];
+  
+  $('body').addClass('-langPreferred_'+langPreferred);
+</script>
+
+<?
 /* Если вам нужно, можете сделать редирект по условию:
 if ($country == 'ua') {
   header('Location: /ua/');
@@ -57,3 +73,17 @@ if ($country == 'ua') {
 */
 </script>
 -->
+  
+<!--  
+<script>
+/* Вы можете проверять не только страну, но и желательный язык для юзера 
+   (ведь физическое нахождение в какой-то стране не значит что желательный язык посетителя — гос язык этой страны.
+
+    if ($('body').hasClass('-langPreferred_ua')) {
+      // Код для украинского языка
+    } else {
+      // Код для всех остальных
+    }  
+*/
+</script>
+-->  
