0cd6c4f1a669276f3062b3af45cee7913031c4c5
Igor Zenich <delka@ideus.biz>
2015-08-03 16:16:38 +0300

    Update php-geo to correct work with v2.2.3 Add more examples of use.

--- php-geo/index.php
+++ php-geo/index.php
@@ -1,25 +1,59 @@
 <?php
 // Если страна не определилась
-$countryDefault = 'ua';
+$COUNTRY_DEFAULT = 'ua';
+$CITY_DEFAULT    = 'kharkiv';
 
 // Детект страны и города по IP
+// Docs: https://sypexgeo.net/ru/docs/
 include('inc/vendor/SxGeo.php');
 $SxGeo = new SxGeo('inc/vendor/SxGeoCity.dat');
 $geo = $SxGeo->getCity($_SERVER['REMOTE_ADDR']);
-$country = ($geo) ? strtolower($geo['country']) : $countryDefault;
-$city = ($geo) ? $geo['city'] : '';
 
-// Редирект по условию
+$country = ($geo) ? strtolower($geo['country']['iso'])  : $COUNTRY_DEFAULT;
+$city    = ($geo) ? strtolower($geo['city']['name_en']) : $CITY_DEFAULT;
+
+/* Если вам нужно, можете сделать редирект по условию:
 if ($country == 'ua') {
   header('Location: /ua/');
 } else {
   header('Location: /en/');
 }
+*/
 ?>
 
+<!-- Добавьте классы-модификаторы в ваш <body> -->
 <body class="-country_<?php echo $country; ?> -city_<?php echo $city; ?>">
 
+<?php  
+/* Внутри шаблона вы можете делать любые проверки:  
   <?php if ($country == 'ua') :?>
+    // Код для Украины
   <?php else: ?>
+    // Код для всех остальных
   <?php endif; ?>
   
+  <?php if ($city == 'kharkiv') :?>
+    //  Код для Харькова
+  <?php else: ?>
+    // Код для всех остальных
+  <?php endif; ?>  
+*/  
+?>
+
+<!--  
+<script>
+/* Проверять можно и из JS:
+    if ($('body').hasClass('-country_ua')) {
+      // Код для Украины
+    } else {
+      // Код для всех остальных
+    }  
+  
+    if ($('body').hasClass('-city_kharkiv')) {
+      //  Код для Харькова
+    } else {
+      // Код для всех остальных
+    }    
+*/
+</script>
+-->
